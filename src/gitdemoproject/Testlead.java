00c2a28165c1e5e65286b3bb2bb29fc8a0366631
Elcot <Elcot@Hasee-PC>
2020-11-26 21:40:20 +0530

    First commit

---
+++ src/gitdemoproject/Testlead.java
@@ -0,0 +1,32 @@
+package gitdemoproject;
+
+public class Testlead 
+{ 
+    int a; 
+    int b; 
+  
+    //Default constructor 
+public Testlead() 
+    { 
+        a = 10; 
+        b = 20; 
+    } 
+      
+    //Method that returns current class instance 
+public Testlead get() 
+    { 
+       return this; 
+    } 
+      
+    //Displaying value of variables a and b 
+public void display() 
+    { 
+        System.out.println("a = " + a + "  b = " + b); 
+    } 
+  
+public static void main(String[] args) 
+    { 
+        Testlead object = new Testlead(); 
+        object.display();
+    } 
+}
